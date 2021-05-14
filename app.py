719f7b699b91ba05c75490c198a2b80306849f19
Matthew <matthew.ruggiero@gmail.com>
2021-05-14 19:30:55 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+# ---- Flask Hello World ---- #
+
+# import the Flask class from the flask package
+from flask import Flask
+
+# create the application object
+app = Flask(__name__)
+
+# error handling
+app.config["DEBUG"] = True
+
+# use the decorator pattern tobytes
+# link the view function to a url
+@app.route("/")
+@app.route("/hello")
+
+# define a view using a function, which returns a string
+def hello_world():
+    return "Hello, World!?!?!?!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+    
+@app.route("/name/<name>")
+def index(name):
+    return "Hello, {}".format(name)
+
+# start the development server using the run() method
+if __name__ == "__main__":
+    app.run()
+
